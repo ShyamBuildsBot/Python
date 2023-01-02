8e03346d9fbf11a7347607fe8db533c12d6fc3b1
Shyam Sundar Shankar <Shyam.dragon89@gmail.com>
2023-01-02 16:08:47 +0000

    Day 01

---
+++ main.py
@@ -0,0 +1,41 @@
+# --------Day 01--------
+
+# 01 Indentation
+# Python uses indentation to indicate a block of code
+# Example
+if 1 > 1:
+  print("Hello!")
+
+# 02 Comments
+# Comments are of Two types: Normal Comment & Multiline Comment
+# Example
+# Normal Comment
+"""
+Multiline Comment
+"""
+
+# 03 Variable
+# Variables in Python doesn't need a Declaration
+# Python is Case-sensitive
+# You can use Single Or Double Quote to Store a String
+# Variable can only have A-Z, 0-9 & _
+# Variable can't begin with Numbers
+
+a = 1
+print(a)
+b = "Shyam"
+print(b)
+
+# 04 Type Casting
+c = str(5)
+print(c)
+d = float(5)
+print(d)
+
+# 05 Find Data Type
+atype = type(a)
+print(atype)
+btype = type(b)
+print(btype)
+
+# --------Day 02--------
