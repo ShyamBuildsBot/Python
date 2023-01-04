0cf6ec0d13a42e5197528c252025ddd699f1b2a8
Shyam Sundar Shankar <Shyam.dragon89@gmail.com>
2023-01-04 19:03:23 +0000

    Lesson 02

---
+++ Lesson 02.py
@@ -0,0 +1,31 @@
+# Variable
+
+"""  
+Variables in Python doesn't need a Declaration
+Python is Case-sensitive
+You can use Single Or Double Quote to Store a String
+Variable can only have A-Z, 0-9 & _
+Variable can't begin with Numbers
+"""
+
+# Example
+a = 1
+print(a)
+b = "Shyam"
+print(b)
+
+# ------------------------------------------------------------------------
+
+# Multiple Variables & Multiple Values
+Pat_Name, Pat_DOS, Pat_DOB = "Shyam", "05/01/1995", "05/10/1920"
+print(Pat_Name)
+print(Pat_DOS)
+print(Pat_DOB)
+
+# ------------------------------------------------------------------------
+
+# Multiple Variables & One Values
+e = f = g = "Sundar"
+print(e)
+print(f)
+print(g)
